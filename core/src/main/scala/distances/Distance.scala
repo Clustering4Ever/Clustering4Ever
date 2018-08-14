0f4963f343c16ea2ca09e30e37175ea46e82d446
gael beck <beck.gael@gmail.com>
2018-08-14 13:42:38 +0200

    gather generic distance class into same file

--- core/src/main/scala/distances/Distance.scala
+++ core/src/main/scala/distances/Distance.scala
@@ -1,5 +1,9 @@
 package clustering4ever.math.distances
 
+import scala.collection.immutable
+import clustering4ever.scala.measurableclass.BinaryScalarVector
+import clustering4ever.scala.clusterizables.ClusterizableM
+
 /**
  * @author Beck Gaël
  * Most general notion of Distance, taking two object of type T and returning a Double
@@ -7,4 +11,34 @@ package clustering4ever.math.distances
 trait Distance[T] extends Serializable
 {
 	def d(obj1: T, obj2: T): Double
+}
+
+trait ContinuousDistances extends Distance[immutable.Seq[Double]]
+{
+	def d(vector1: immutable.Seq[Double], vector2: immutable.Seq[Double]): Double
+}
+
+trait BinaryDistance[T <: immutable.Traversable[Int]] extends Distance[T]
+{
+	def d(vector1: T, vector2: T): Double
+}
+
+trait BinaryDistanceSeq extends BinaryDistance[immutable.Seq[Int]]
+{
+	def d(vector1: immutable.Seq[Int], vector2: immutable.Seq[Int]): Double
+}
+
+trait BinaryDistanceVector extends BinaryDistance[immutable.Vector[Int]]
+{
+	def d(vector1: immutable.Vector[Int], vector2: immutable.Vector[Int]): Double
+}
+
+trait MixtDistance extends Distance[BinaryScalarVector]
+{
+	def d(vector1: BinaryScalarVector, vector2: BinaryScalarVector): Double
+}
+
+trait MixtDistanceClusterizable[ID, Obj] extends Distance[ClusterizableM[ID, Obj]]
+{
+	def d(vector1: ClusterizableM[ID, Obj], vector2: ClusterizableM[ID, Obj]): Double
 }
