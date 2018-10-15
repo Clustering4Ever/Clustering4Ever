6abf78de6d2ef7cf6fb9b954f5d1483dc0739bf7
beckgael <beck.gael@gmail.com>
2018-10-15 09:53:45 +0200

    avoid unnecessary cast in vectorizable

--- core/src/main/scala/basicClasses/Vectorizables.scala
+++ core/src/main/scala/basicClasses/Vectorizables.scala
@@ -8,21 +8,18 @@ trait Vectorizable[V] extends Serializable {
 	def toVector(): V
 }
 
-abstract class VectorizableObj[O, V](val obj: O, vectorizableFct: Option[O => V] = None) extends Vectorizable[V] {
-	def toVector(): V = {
-		if( ! vectorizableFct.isDefined ) obj.asInstanceOf[V]
-		else vectorizableFct.get(obj)
-	}
+abstract class VectorizableObj[O, V](val obj: O, vectorizableFct: O => V) extends Vectorizable[V] {
+	def toVector(): V = vectorizableFct(obj)
 }
 
-class RealVectorizable[O, V <: Seq[Double]](obj: O, vectorizableFct: Option[O => V] = None) extends VectorizableObj[O, V](obj, vectorizableFct)
+class RealVectorizable[O, V <: Seq[Double]](obj: O, vectorizableFct: O => V = identity _) extends VectorizableObj[O, V](obj, vectorizableFct)
 
-class RealVector[V <: Seq[Double]](vector: V) extends RealVectorizable[V, V](vector, None)
+class RealVector[V <: Seq[Double]](vector: V) extends RealVectorizable[V, V](vector, identity)
 
-class BinaryVectorizable[O, V <: Seq[Int]](obj: O, vectorizableFct: Option[O => V] = None) extends VectorizableObj[O, V](obj, vectorizableFct)
+class BinaryVectorizable[O, V <: Seq[Int]](obj: O, vectorizableFct: O => V = identity _) extends VectorizableObj[O, V](obj, vectorizableFct)
 
-class BinaryVector[V <: Seq[Int]](vector: V) extends BinaryVectorizable[V, V](vector, None)
+class BinaryVector[V <: Seq[Int]](vector: V) extends BinaryVectorizable[V, V](vector, identity)
 
-class MixtVectorizable[O, Vb <: Seq[Int], Vs <: Seq[Double], V <: BinaryScalarVector[Vb, Vs]](obj: O, vectorizableFct: Option[O => V] = None) extends VectorizableObj[O, V](obj, vectorizableFct)
+class MixtVectorizable[O, Vb <: Seq[Int], Vs <: Seq[Double], V <: BinaryScalarVector[Vb, Vs]](obj: O, vectorizableFct: O => V = identity _) extends VectorizableObj[O, V](obj, vectorizableFct)
 
-class MixtVector[Vb <: Seq[Int], Vs <: Seq[Double], V <: BinaryScalarVector[Vb, Vs]](obj: V) extends MixtVectorizable[V, Vb, Vs, V](obj, None)
+class MixtVector[Vb <: Seq[Int], Vs <: Seq[Double], V <: BinaryScalarVector[Vb, Vs]](obj: V) extends MixtVectorizable[V, Vb, Vs, V](obj, identity)
