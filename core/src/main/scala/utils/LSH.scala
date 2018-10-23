53974ee04ea30acd58f0cf87b11873851345531b
beckgael <beck.gael@gmail.com>
2018-10-23 14:52:35 +0200

    generalize hashing method with class

--- core/src/main/scala/utils/LSH.scala
+++ core/src/main/scala/utils/LSH.scala
@@ -3,24 +3,36 @@ package clustering4ever.util
  * @author Beck Gaël
  */
 import scala.collection.mutable
+import scala.util.Random
+
+trait HashingArgs extends Serializable
+/**
+ * Trait regrouping various hashing methods
+ */
+trait Hasher[V <: Seq[Double]] extends Serializable {
+  def hf(v: V): Double
+}
 /**
  * A basic implementation of Locality Sensitive Hashing
  */
-object LSH {
+class LSH(val dim: Int, val w: Double = 1D) extends Hasher[mutable.ArrayBuffer[Double]] {
+
+  val b = Random.nextDouble * w
   /**
    * Create a random vector where component are taken on normal law N(0,1) for LSH
    */
-  def obtainHashVector(dim: Int): mutable.ArrayBuffer[Double] = mutable.ArrayBuffer.fill(dim)(scala.util.Random.nextGaussian)
+  val hashVector = mutable.ArrayBuffer.fill(dim)(Random.nextGaussian)
   /**
    *  Generate the hash value for a given vector x depending on w, b, hashVector
    */
-  def hf(x: mutable.ArrayBuffer[Double], w: Double, b: Double, hv: mutable.ArrayBuffer[Double]): Double = {
+  def hf(x: mutable.ArrayBuffer[Double]): Double = {
   	var s = 0D
     var i = 0
     while( i < x.size ) {
-      s += x(i) * hv(i)
+      s += x(i) * hashVector(i)
       i += 1
     }
   	( s + b ) / w
   }
+
 }
