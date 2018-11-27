a042f053f611d2c2aa19e721a6c6147e718d21d6
andriantsiory <afanevadina@gmail.com>
2018-11-27 12:31:24 +0100

    new biclustering

---
+++ clustering/scala/src/main/scala/Clustering Algorithms/Tensor/Multiple_Biclusters.scala
@@ -0,0 +1,17 @@
+package org.clustering4ever.scala.clustering.tensor
+/**
+ * @author ANDRIANTSIORY Dina Faneva, Beck Gaël
+ */
+import breeze.linalg._
+import breeze.math._
+import breeze.numerics._
+import scala.collection.mutable
+import breeze.linalg.svd.SVD
+import breeze.stats._
+import org.clustering4ever.clustering.ClusteringAlgorithm
+/**
+ *
+ */
+class MultipleBiclusters(val yolo: mutable.ArrayBuffer[Int]) extends ClusteringAlgorithm {
+
+}
