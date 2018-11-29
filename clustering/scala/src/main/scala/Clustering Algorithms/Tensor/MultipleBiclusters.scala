b4981d126bc81567e57562af2517d8f4e40e8eec
andriantsiory <afanevadina@gmail.com>
2018-11-29 17:01:58 +0100

    modif

--- clustering/scala/src/main/scala/Clustering Algorithms/Tensor/MultipleBiclusters.scala
+++ clustering/scala/src/main/scala/Clustering Algorithms/Tensor/MultipleBiclusters.scala
@@ -19,91 +19,92 @@ class MultipleBiclusters(val k1: Array[Int], val k2: Array[Int]
  def run(data: mutable.ArrayBuffer[DenseMatrix[Double]]) = {
 
 
-    val m = data.length
-    val n1 = data.head.rows
-    val n2 = data.head.cols
-    val timeColumn = DenseMatrix.zeros[Double](m, n2)  
-    val timeRow = DenseMatrix.zeros[Double](m, n1)
-    var columnMatrix = DenseMatrix.zeros[Double](n2,n2)
-    var rowMatrix = DenseMatrix.zeros[Double](n1,n1)
+      val m = data.length
+      val n1 = data.head.rows  //nomber of individuals
+      val n2 = data.head.cols   //number of columns
+      val timeColumn = DenseMatrix.zeros[Double](m,n2)  
+      val timeRow = DenseMatrix.zeros[Double](m,n1)
 
 
      @annotation.tailrec
-     def matrice1(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
+     def matriceColumnSet(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
         DenseMatrix[Double] = {
-        if (j < t(0).cols && k < t.length){
+        if (j < t.head.cols && k < t.length){
         m(k,j) = t(k)(i,j);
-        matrice1(t,m, c, i, j, k + 1)
+        matriceColumnSet(t,m, c, i, j, k + 1);
       }
-      else if (k == t.length && j < t(0).cols){
-        matrice1(t, m, c, i, j+1 , 0)
+      else if (k == t.length && j < t.head.cols){
+        matriceColumnSet(t, m, c, i, j+1 , 0);
       }
 
-      else if (i < t(0).rows - 1){
+      else if (i < t.head.rows - 1){
         c += cov(m)
-        matrice1(t, m, c, i+1, 0, 0)
+        matriceColumnSet(t, m, c, i+1, 0, 0);
       }else {
         c += cov(m)
       }
     }
 
-
     @annotation.tailrec
-    def matrice2(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
+    def matriceRowSet(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
         DenseMatrix[Double] = {
-        if (i < t(0).rows && k < t.length){
-        m(k,i) = t(k)(i,j)
-        matrice2(t,m, c, i, j, k + 1)
+        if (i < t.head.rows && k < t.length){
+        m(k,i) = t(k)(i,j);
+        matriceRowSet(t,m, c, i, j, k + 1);
       }
-      else if (k == t.length && i < t(0).rows){
-        matrice2(t, m, c, i+1, j , 0)
+      else if (k == t.length && i < t.head.rows){
+        matriceRowSet(t, m, c, i+1, j , 0);
       }
 
       else if (j < t(0).cols - 1){
         c += cov(m)
-        matrice2(t, m, c, 0, j+1, 0)
+        matriceRowSet(t, m, c, 0, j+1, 0);
       }else {
         c += cov(m)
       }
     }
 
-    columnMatrix = matrice1(data, timeColumn, columnMatrix, 0, 0, 0 )
+
+    val columnMatrix = matriceColumnSet(tensor, timeColumn, DenseMatrix.zeros[Double](n2,n2), 0, 0, 0 )
     val svd.SVD(u1,eigValue,eigVector) = svd(columnMatrix)
-    val column_eigvalue = eigValue.toArray
+   // val column_eigvalue = eigValue.toArray
     val column_eigvector = eigVector.t
          
-    rowMatrix = matrice2(data, timeRow, rowMatrix, 0, 0, 0 )
+ 
+
+    val rowMatrix = matriceRowSet(tensor, timeRow, DenseMatrix.zeros[Double](n1,n1), 0, 0, 0 )
     val svd.SVD(u2,eigValue2,eigVector2) = svd(rowMatrix)
-    val row_eigvalue = eigValue2.toArray
+   // val row_eigvalue = eigValue2.toArray
     val row_eigvector = eigVector2.t
        @annotation.tailrec
-     def geTheTopkIndices(m: DenseMatrix[Double], k:Array[Int], v: ListBuffer[Array[Int]],  i: Int): ListBuffer[Array[Int]] = {
+     def geTTheTopkIndices(m: DenseMatrix[Double], k: Array[Int], v: ListBuffer[Array[Int]],  i: Int): ListBuffer[Array[Int]] = {
       
         if (i < k.length){
           val g = k(i)
-           v += m(::,i).map(x => abs(x)).toArray.zipWithIndex.sortWith((x, y) => x._1 > y._1).take(g).map(x => x._2) 
+          v += m(::,i).map(x => abs(x)).toArray.zipWithIndex.sortWith((x, y) => x._1 > y._1).take(g).map(x => x._2) 
                    
-          geTheTopkIndices(m, k, v, i+1)
+          geTTheTopkIndices(m, k, v, i+1)
         }
         else {
-           v
+          v
         }
       }
       
       var uu = ListBuffer[Array[Int]]()
-      val row = geTheTopkIndices(row_eigvector, k1, uu, 0)
+      val row = geTTheTopkIndices(row_eigvector, k1, uu, 0)
 
 
       var ww = ListBuffer[Array[Int]]()
-      val column = geTheTopkIndices(column_eigvector, k2, ww, 0)
+      val column = geTTheTopkIndices(column_eigvector, k2, ww, 0)
 
     
-    def intersection(int_row:Array[Int], a: ListBuffer[Array[Int]], int_column:Array[Int], b: ListBuffer[Array[Int]], i: Int):  Array[Array[Int]] ={
+    def intersection(int_row:Array[Int], a: ListBuffer[Array[Int]], int_column:Array[Int], b: ListBuffer[Array[Int]], i: Int):
+    Array[Array[Int]] ={
       if (i < a.length){
        val intRow = int_row.intersect(a(i))      // Intersection is a decreasing function
        val intCol = int_column.intersect(b(i))
-        println("\n\n Bicluster number "+ i +": \n individuals "+ row(i).toSet + "\n features "+ column(i).toSet )
-        intersection(intRow, a, intCol, b, i+1)
+       // println("\n\n Bicluster number "+ i +": \n individuals "+ row(i).toSet + "\n features "+ column(i).toSet )
+       intersection(intRow, a, intCol, b, i+1)
       }
       else {
        Array(int_row, int_column) 
@@ -111,13 +112,23 @@ class MultipleBiclusters(val k1: Array[Int], val k2: Array[Int]
     }
           
     val intersections = intersection(row(0), row, column(0), column, 0)
-    println("\n Individuals intersection  is :"+ intersections(0).toSet + " \n length = "+ intersections(0).length+ 
-            "\n Features  intersection :" + intersections(1).toSet +" \n length = "+ intersections(1).length )
+    //println("\n Individuals intersection  is :"+ intersections(0).toSet + " \n length = "+ intersections(0).length+"\n Features  intersection :" + intersections(1).toSet +" \n length = "+ intersections(1).length )
    
     //The top five of eigenvalues 
-    println("\n Top five eigenvalues of the matrix C1: "+ row_eigvalue.take(5).toList
-            +"\n Top five Eigenvalues of the matrix C2: "+ column_eigvalue.take(5).toList)
-    
+    //println("\n Top five eigenvalues of the matrix C1: "+ row_eigvalue.take(5).toList+"\n Top five Eigenvalues of the matrix C2: "+ column_eigvalue.take(5).toList)
+    def result(r: ListBuffer[Array[Int]], c: ListBuffer[Array[Int]], rc:ListBuffer[Array[Array[Int]]], i:Int, l: Int ): ListBuffer[Array[Array[Int]]] = {
+      if (i < l){
+        rc += Array(r(i), c(i)) 
+        result(r, c, rc, i+1, l)
+      } 
+      else {
+        rc
+      }
+    }
+    val fin = ListBuffer[Array[Array[Int]]]()  
+    val resultats = result(row, column, fin, 0, k1.length)
+    (resultats, intersections)
+  
   }
 }  
  
--- clustering/scala/src/main/scala/Clustering Algorithms/Tensor/RecursiveBiclusters.scala
+++ clustering/scala/src/main/scala/Clustering Algorithms/Tensor/RecursiveBiclusters.scala
@@ -17,64 +17,60 @@ class RecursiveBiclusters ( val l1: Array[Int],
     
     def one_bicluster(k1: Int, k2: Int, tensor1: mutable.ArrayBuffer[DenseMatrix[Double]])={
       
-      val m = tensor1.length;
-      val n1 = tensor1(0).rows;  //nomber of individuals
-      val n2 = tensor1(0).cols;   //number of columns
-      val timeColumn = DenseMatrix.zeros[Double](m,n2);  
-      val timeRow = DenseMatrix.zeros[Double](m,n1);
-      var columnMatrix = DenseMatrix.zeros[Double](n2,n2);
-      var rowMatrix = DenseMatrix.zeros[Double](n1,n1);
+      val mt = tensor1.length
+      val n1 = tensor1(0).rows  //nomber of individuals
+      val n2 = tensor1(0).cols   //number of columns
+      val timeColumn = DenseMatrix.zeros[Double](mt,n2) 
+      val timeRow = DenseMatrix.zeros[Double](mt,n1)
 
 
      @annotation.tailrec
-     def matrice1(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
+     def matriceColumnSet(t:ListBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
         DenseMatrix[Double] = {
         if (j < t(0).cols && k < t.length){
         m(k,j) = t(k)(i,j);
-        matrice1(t,m, c, i, j, k + 1);
+        matriceColumnSet(t,m, c, i, j, k + 1)
       }
       else if (k == t.length && j < t(0).cols){
-        matrice1(t, m, c, i, j+1 , 0);
+        matriceColumnSet(t, m, c, i, j+1 , 0)
       }
 
       else if (i < t(0).rows - 1){
         c += cov(m)
-        matrice1(t, m, c, i+1, 0, 0);
+        matriceColumnSet(t, m, c, i+1, 0, 0)
       }else {
         c += cov(m)
-        c;
       }
     }
 
     @annotation.tailrec
-    def matrice2(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
+    def matriceRowSet(t:ListBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
         DenseMatrix[Double] = {
         if (i < t(0).rows && k < t.length){
-        m(k,i) = t(k)(i,j);
-        matrice2(t,m, c, i, j, k + 1);
+        m(k,i) = t(k)(i,j)
+        matriceRowSet(t,m, c, i, j, k + 1)
       }
       else if (k == t.length && i < t(0).rows){
-        matrice2(t, m, c, i+1, j , 0);
+        matriceRowSet(t, m, c, i+1, j , 0)
       }
 
       else if (j < t(0).cols - 1){
         c += cov(m)
-        matrice2(t, m, c, 0, j+1, 0);
+        matriceRowSet(t, m, c, 0, j+1, 0)
       }else {
-        c += cov(m);
-        c;
+        c += cov(m)
       }
     }
 
-   columnMatrix = matrice1(tensor1, timeColumn, columnMatrix, 0, 0, 0 )
+    val columnMatrix = matriceColumnSet(tensor1, timeColumn, DenseMatrix.zeros[Double](n2,n2), 0, 0, 0 )
     val svd.SVD(u1,eigValue,eigVector) = svd(columnMatrix)
-    val column_eigvalue = eigValue.toArray
+   // val column_eigvalue = eigValue.toArray
     val column_eigvector = eigVector.t
          
  
-      rowMatrix = matrice2(tensor1, timeRow, rowMatrix, 0, 0, 0 )
+    val rowMatrix = matriceRowSet(tensor1, timeRow, DenseMatrix.zeros[Double](n1,n1), 0, 0, 0 )
     val svd.SVD(u2,eigValue2,eigVector2) = svd(rowMatrix)
-    val row_eigvalue = eigValue2.toArray
+  //  val row_eigvalue = eigValue2.toArray
     val row_eigvector = eigVector2.t
    
      def geTTheTopkIndices(m: DenseMatrix[Double], k: Int):
@@ -90,10 +86,10 @@ class RecursiveBiclusters ( val l1: Array[Int],
       
      println("\n\n bicluster number "+": \n rows "+ row.toList + "\n column "+ column.toList )
   
-    def annulation(t:mutable.ArrayBuffer[DenseMatrix[Double]],jf: Array[Int], js: Array[Int]): 
+    def annulation(t:mutable.ArrayBuffer[DenseMatrix[Double]],jf: Array[Int], js: Array[Int], m: Int): 
     mutable.ArrayBuffer[DenseMatrix[Double]] = { 
   
-      for (k <- 0 until 4) {
+      for (k <- 0 until m) {
           for (i <- jf){
             for (j <- js){
                   t(k)(i, j) = 0 
@@ -102,19 +98,21 @@ class RecursiveBiclusters ( val l1: Array[Int],
       }
       t
     } 
-    val tensor_remain = annulation(tensor1, row, column)
+      var tensor_remain = annulation(tensor1, row, column, mt)
       
-    tensor_remain
-      
+      (tensor_remain, Array(row, column) )
     }
     
-  //  var nombre = 0
     var r1 =ListBuffer[mutable.ArrayBuffer[DenseMatrix[Double]]]()
-    r1 += data
+    r1 += tens
+    var result = ListBuffer[Array[Array[Int]]]()
     
     for (nombre <- 0 until l1.length) {
-       r1 += one_bicluster(l1(nombre), l2(nombre), r1(nombre))
-      }   
+      val (t1, re1) = one_bicluster(l1(nombre), l2(nombre), r1(nombre))
+      r1 += t1
+      result += re1
+      }
+    result
   }
 }  
 
