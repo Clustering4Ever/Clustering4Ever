27d8ba4f673111cc044455c2966a0b0872bef61b
Beck Gaël <beck.gael@gmail.com>
2018-12-04 14:45:39 +0100

    Dina's works on tensor

--- clustering/scala/src/main/scala/Clustering Algorithms/Tensor/MultipleBiclusters.scala
+++ clustering/scala/src/main/scala/Clustering Algorithms/Tensor/MultipleBiclusters.scala
@@ -15,7 +15,7 @@ import org.clustering4ever.clustering.ClusteringAlgorithm
  */
 class MultipleBiclusters(val k1: Array[Int], val k2: Array[Int]) extends ClusteringAlgorithm {
 
-  def run(data: mutable.ArrayBuffer[DenseMatrix[Double]]): TensorBiclusteringModel = {
+  def run(data: mutable.ArrayBuffer[DenseMatrix[Double]]): Unit = {
 
     val m = data.size
     val n1 = data.head.rows
@@ -26,50 +26,50 @@ class MultipleBiclusters(val k1: Array[Int], val k2: Array[Int]) extends Cluster
     var rowMatrix = DenseMatrix.zeros[Double](n1,n1)
 
      @annotation.tailrec
-     def matrice1(t: mutable.ArrayBuffer[DenseMatrix[Double]], m: DenseMatrix[Double], c: DenseMatrix[Double], i: Int, j: Int , k: Int): 
+     def matriceColumnSet(t: mutable.ArrayBuffer[DenseMatrix[Double]], m: DenseMatrix[Double], c: DenseMatrix[Double], i: Int, j: Int , k: Int): 
         DenseMatrix[Double] = {
         if(j < t.head.cols && k < t.length) {
         m(k, j) = t(k)(i,j)
-        matrice1(t,m, c, i, j, k + 1)
+        matriceColumnSet(t,m, c, i, j, k + 1)
       }
       else if(k == t.length && j < t.head.cols) {
-        matrice1(t, m, c, i, j+1 , 0)
+        matriceColumnSet(t, m, c, i, j+1 , 0)
       }
       else if(i < t.head.rows - 1) {
         c += cov(m)
-        matrice1(t, m, c, i+1, 0, 0)
+        matriceColumnSet(t, m, c, i+1, 0, 0)
       }
       else {
         c += cov(m)
       }
     }
 
-
     @annotation.tailrec
-    def matrice2(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): 
-        DenseMatrix[Double] = {
-        if (i < t(0).rows && k < t.length){
+    def matriceRowSet(t:mutable.ArrayBuffer[DenseMatrix[Double]], m:DenseMatrix[Double], c:DenseMatrix[Double], i: Int, j: Int , k: Int): DenseMatrix[Double] = {
+      if (i < t.head.rows && k < t.length){
         m(k,i) = t(k)(i,j)
-        matrice2(t,m, c, i, j, k + 1)
+        matriceRowSet(t,m, c, i, j, k + 1)
       }
       else if (k == t.length && i < t(0).rows){
-        matrice2(t, m, c, i+1, j , 0)
+        matriceRowSet(t, m, c, i + 1, j , 0)
       }
 
-      else if (j < t(0).cols - 1){
+      else if (j < t.head.cols - 1){
         c += cov(m)
-        matrice2(t, m, c, 0, j+1, 0)
-      }else {
+        matriceRowSet(t, m, c, 0, j + 1, 0)
+      }
+      else {
         c += cov(m)
       }
     }
 
-    columnMatrix = matrice1(data, timeColumn, columnMatrix, 0, 0, 0 )
+    
+    columnMatrix = matriceColumnSet(data, timeColumn, columnMatrix, 0, 0, 0 )
     val svd.SVD(u1, eigValue, eigVector) = svd(columnMatrix)
     val columnEigvalue = eigValue.toArray
     val columnEigvector = eigVector.t
          
-    rowMatrix = matrice2(data, timeRow, rowMatrix, 0, 0, 0 )
+    rowMatrix = matriceRowSet(data, timeRow, rowMatrix, 0, 0, 0 )
     val svd.SVD(u2,eigValue2,eigVector2) = svd(rowMatrix)
     val rowEigvalue = eigValue2.toArray
     val rowEigvector = eigVector2.t
@@ -94,31 +94,44 @@ class MultipleBiclusters(val k1: Array[Int], val k2: Array[Int]) extends Cluster
     val column = obtainTopkIndicesDM(columnEigvector, k2, ww, 0)
 
     @annotation.tailrec
-    def intersection(intRow: Array[Int], a: ListBuffer[Array[Int]], intColumn: Array[Int], b: ListBuffer[Array[Int]], i: Int):  Array[Array[Int]] ={
-      if (i < a.length){
-       val intRow2 = intRow.intersect(a(i))      // Intersection is a decreasing function
-       val intCol = intColumn.intersect(b(i))
-        println("\n\n Bicluster number "+ i +": \n individuals "+ row(i).toSet + "\n features "+ column(i).toSet )
-        intersection(intRow2, a, intCol, b, i+1)
-      }
+     def intersection(int_row:Array[Int], a: ListBuffer[Array[Int]], int_column:Array[Int], b: ListBuffer[Array[Int]], i: Int):
+        Array[Array[Int]] ={
+          if (i < a.length){
+           val intRow = int_row.intersect(a(i))      // Intersection is a decreasing function
+           val intCol = int_column.intersect(b(i))
+           intersection(intRow, a, intCol, b, i+1)
+          }
+          else {
+           Array(int_row, int_column) 
+          }
+        }
+          
+    val Array(rowIntersection, columnIntersection) = intersection(row.head, row, column.head, column, 0)
+   
+    @annotation.tailrec
+    def result(r: mutable.ListBuffer[Array[Int]], c: mutable.ListBuffer[Array[Int]], rc:mutable.ListBuffer[Array[Array[Int]]], i:Int, l: Int ): mutable.ListBuffer[Array[Array[Int]]] = {
+      if (i < l){
+        rc += Array(r(i), c(i)) 
+        result(r, c, rc, i+1, l)
+      } 
       else {
-       Array(intRow, intColumn) 
+        rc
       }
     }
-          
-    val Array(rowIndexes, columnIndexes) = intersection(row.head, row, column.head, column, 0)
-    // println("\n Individuals intersection  is :"+ intersections(0).toSet + " \n length = "+ intersections(0).length + "\n Features  intersection :" + intersections(1).toSet +" \n length = "+ intersections(1).length )
-   
-    //The top five of eigenvalues 
-    // println("\n Top five eigenvalues of the matrix C1: "+ rowEigvalue.take(5).toList + "\n Top five Eigenvalues of the matrix C2: "+ columnEigvalue.take(5).toList)
-    
-    new TensorBiclusteringModel(rowIndexes, columnIndexes)
+    val fin = mutable.ListBuffer[Array[Array[Int]]]()
 
+    val resultats = result(row, column, fin, 0, k1.length)
+
+    //new TensorBiclusteringModel(rowIndexes, columnIndexes)
+
+    (resultats, (rowIntersection, columnIntersection) ) 
+
+    //Unit
   }
 }  
  
 object MultipleBiclusters {
   
-  def train(k1:Array[Int], k2:Array[Int], data: mutable.ArrayBuffer[DenseMatrix[Double]]): TensorBiclusteringModel = (new MultipleBiclusters(k1, k2)).run(data)
+  //def train(k1:Array[Int], k2:Array[Int], data: mutable.ArrayBuffer[DenseMatrix[Double]]): TensorBiclusteringModel = (new MultipleBiclusters(k1, k2)).run(data)
 
 }
