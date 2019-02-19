1ab9b9f2dc704ff9f5a4f621acf3e39982e08d14
beckgael <beck.gael@gmail.com>
2019-02-19 18:47:21 +0100

    maj

--- clustering/scala/src/main/scala/ClusteringAlgorithms/AntTree/NewTree.scala
+++ clustering/scala/src/main/scala/ClusteringAlgorithms/AntTree/NewTree.scala
@@ -17,7 +17,8 @@ import scala.collection.{mutable, immutable}
 import scalax.collection.mutable.{Graph => MutableGraph}
 
 
-trait AntTreeAlgoModelAncestor[V <: GVector[V], D <: Distance[V]]{
+trait AntTreeAlgoModelAncestor[V <: GVector[V], D <: Distance[V]] {
+
   val tree: MutableGraph[(Long, Option[V]), UnDiEdge]
 
   final def allSuccessors(xpos: (Long, Option[V])): immutable.Set[(Long, Option[V])] = {
@@ -25,7 +26,7 @@ trait AntTreeAlgoModelAncestor[V <: GVector[V], D <: Distance[V]]{
     node.withSubgraph().map(_.value).toSet - node
   }
 
-  final def directSuccessors(xpos: (Long, Option[V])): Set[tree.NodeT] = tree.get(xpos).diSuccessors
+  final def directSuccessors(xpos: (Long, Option[V])): immutable.Set[(Long, Option[V])] = tree.get(xpos).diSuccessors.map(_.value)
 
 }
 
@@ -50,10 +51,6 @@ trait AntTreeModelAncestor[V <: GVector[V], D <: Distance[V]] extends Clustering
   }
 */
 }
-// La classe en dehors peux être mieux pour de futur applications si il faut la complexifier, pour l'instant elle a juste l'idée
-final class Ant2[O, V <: GVector[V], Cz[B, C <: GVector[C]] <: Clusterizable[B, C, Cz]](final val clusterizable: Option[Cz[O, V]], final var firstTime: Boolean = true) {
-    final val id = if(clusterizable.isDefined) clusterizable.get.id else Long.MinValue
-}
 /**
  * @tparm V
  * @tparm D
@@ -66,12 +63,6 @@ trait AntTreeAncestor[V <: GVector[V], D <: Distance[V], CM <: AntTreeModelAnces
 
   final def train[O, Cz[B, C <: GVector[C]] <: Clusterizable[B, C, Cz], GS[X] <: GenSeq[X]](data: GS[Cz[O, V]]): Unit = {
 
-    // trait AntTreeObject {
-    // Possible de faire avec val mais il faudrait une méthode
-    // var firstTime = true
-    // def update
-    // }
-
     class Ant(final val clusterizable: Option[Cz[O, V]], final var firstTime: Boolean = true) {
       final val id = if(clusterizable.isDefined) clusterizable.get.id else Long.MinValue
     }
@@ -82,8 +73,6 @@ trait AntTreeAncestor[V <: GVector[V], D <: Distance[V], CM <: AntTreeModelAnces
 
     val ants = {
       val dataToAnts = scala.util.Random.shuffle(data.map(cz => new Ant(Some(cz))).toIterator)
-      // evites de zip quand tu peux faire direct un map ça économise des parcours de data
-      // dataToAnts.map(_.id).toArray.zip(dataToAnts).toMap + (support.id -> support)
       dataToAnts.map( ant => (ant.id, ant) ).toMap + (support.id -> support)
     }
 
@@ -94,28 +83,24 @@ trait AntTreeAncestor[V <: GVector[V], D <: Distance[V], CM <: AntTreeModelAnces
     def longToNode(l: Long): (Long, Option[V]) = (l, Option(ants(l).clusterizable.get.v))
 
     def connect(xi: Long, xpos: Long): Unit = {
-
       tree += longToNode(xpos) ~> longToNode(xi)
     }
 
     def disconnect(xpos: Long): Unit = {
       val node = tree.get(longToNode(xpos))
       val successors = allSuccessors(longToNode(xpos)) + node
-      // for (key <- successors) ants(key).firstTime = true
       successors.foreach(key => ants(key._1).firstTime = true)
       tree --= successors.asInstanceOf[Set[tree.NodeT]]
     }
 
     def dissimilarValue(xpos: Long): Double = {
       val successors = directSuccessors(longToNode(xpos))
-
-      // successors.map(successor => metric.d(ants(xpos).clusterizable.get.v, ants(successor).clusterizable.get.v)).min
       val minSuccessor = successors.minBy(successor => metric.d(ants(xpos).clusterizable.get.v, ants(successor._1).clusterizable.get.v))
       metric.d(ants(xpos).clusterizable.get.v, ants(minSuccessor._1).clusterizable.get.v)
     }
 
     @annotation.tailrec
-    def findxplus(xi: Long, xplus: Long, successors: Set[tree.NodeT]): Long = {
+    def findxplus(xi: Long, xplus: Long, successors: Set[(Long, Option[V])]): Long = {
       if(successors.isEmpty) xplus
       else {
         if(
@@ -132,7 +117,7 @@ trait AntTreeAncestor[V <: GVector[V], D <: Distance[V], CM <: AntTreeModelAnces
     }
 
     def mostSimilarNode(xi: Long, xpos: Long): Long = {
-      val successors = directSuccessors(longToNode(xpos)).asInstanceOf[Set[tree.NodeT]]
+      val successors = directSuccessors(longToNode(xpos))
       findxplus(xi, successors.head._1, successors.tail)
     }
 
